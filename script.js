1ca1ce8efac1a44572014293a40ca8c18b9526c8
katie-soft <analyst.meo@gmail.com>
2023-11-04 19:22:11 +0300

    feat: add form validation

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
+/* Handle form */
+
 const form = document.querySelector("#form");
 const submitButton = form.querySelector("#submit-button");
-const copyButton = document.querySelector("#copy-button");
-const resultField = document.querySelector("#result");
+const requiredElements = form.querySelectorAll("[required]");
+
+function changeButtonState(button, isFormValid) {
+  if (isFormValid) {
+    button.classList = "button";
+    button.addEventListener('click', handleFormSubmit);
+  } else {
+    button.classList = "button button_inactive";
+    button.removeEventListener('click', handleFormSubmit);
+  }
+}
+
+function validateForm() {
+  changeButtonState(submitButton, false);
+
+  let isFormValid = false;
+
+  requiredElements.forEach(element => {
+    if (element.value.length === 0) {
+      isFormValid = false;
+      return isFormValid;
+    } else {
+      isFormValid = true;
+    }
+  })
+  
+  changeButtonState(submitButton, isFormValid);
+}
+
+requiredElements.forEach(element => {
+  element.addEventListener('input', validateForm);
+})
 
 function handleFormSubmit(event) {
   event.preventDefault();
@@ -14,8 +46,6 @@ function handleFormSubmit(event) {
     data[name] = value.trim();
   })
 
-  data['utm-type'] = form.querySelector("input[type=radio]:checked").value;
-
   if (data.link.at(-1) === '/') {
     data.link = data.link.slice(0, -1);
   }
@@ -23,18 +53,28 @@ function handleFormSubmit(event) {
   const regex = /\-/gm;
   data['article-url'] = data['article-url'].replace(regex, "_");
 
+  data['utm-type'] = form.querySelector("input[type=radio]:checked").value;
+
+  data.product = data.link.split("/").slice(-1);
+
   result = `${data.link}/?internal_source=tsecrets-${data['article-url']}-${data['utm-type']}-${data.product}`;
 
   resultField.value = result; 
   }
+
+/* Copy result */
+
+const copyButton = document.querySelector("#copy-button");
+const resultField = document.querySelector("#result");
 
 function copy() {
   resultField.select();
   document.execCommand("copy");
 }
 
-submitButton.addEventListener('click', handleFormSubmit);
 copyButton.addEventListener('click', copy);
+
+/* Custom select */
 
 const customSelect = form.querySelector(".custom-select");
 customSelect.addEventListener('click', () => {
