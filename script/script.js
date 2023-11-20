ddbe4f509fa4917fbd09079952b2b77b3a9fbefa
katie-soft <analyst.meo@gmail.com>
2023-11-20 21:35:56 +0300

    fix: change validation

--- script/script.js
+++ script/script.js
@@ -1,119 +1,3 @@
-/* Handle form */
-
-const form = document.querySelector('#form');
-const requiredElements = form.querySelectorAll('[required]');
-const submitButton = form.querySelector('#submit-button');
-const clearButton = form.querySelector('#clear-button');
-
-function changeButtonState(button, isFormValid) {
-  if (isFormValid) {
-    button.classList = 'button';
-  } else {
-    button.classList = 'button button_inactive';
-  }
-}
-
-function validateForm() {
-  changeButtonState(submitButton, false);
-
-  let isFormValid = false;
-
-  requiredElements.forEach(element => {
-    if (element.value.length === 0) {
-      isFormValid = false;
-      return isFormValid;
-    } else {
-      isFormValid = true;
-    }
-  })
-
-  if (form.querySelector('#product-type').checked) {
-    isFormValid = isFormValid && !!form.querySelector('#special-product').value.length;
-  }
-
-  changeButtonState(submitButton, isFormValid);
-  return isFormValid;
-}
-
-function checkFormIsFilled() {
-  const formElements = Array.from(form.elements);
-
-  for (let i = 0; i < formElements.length; i++) {
-    if (
-      formElements[i].type === 'text' && formElements[i].value.length ||
-      formElements[i].type === 'hidden' && formElements[i].value.length ||
-      formElements[i].id === 'product-type' && formElements[i].checked) {
-      changeButtonState(clearButton, true);
-      break;
-    }
-    changeButtonState(clearButton, false);
-  }
-}
-
-function handleFormSubmit(event) {
-  event.preventDefault();
-
-  if (validateForm()) {
-    let result = '';
-    const data = {};
-
-    Array.from(form.elements).forEach((element) => {
-      const { name, type } = element;
-      const value = type === 'checkbox' ? element.checked : element.value.trim();
-      data[name] = value;
-    })
-
-    data.link = trimFromSlash(data.link);
-
-    const regex = /\-/gm;
-    data.category = data.category.replace(regex, '_');
-
-    data['utm-type'] = form.querySelector('input[type=radio]:checked').value;
-
-    if (data['product-type']) {
-      data.product = data['special-product'];
-    } else {
-      data.product = getSlug(data.link);
-    }
-
-    result = `${data.link}/?internal_source=tsecrets-${data.category}-${data['utm-type']}-${data.product}`;
-
-    resultField.value = result;
-    resultBlock.classList.add('block_result_visible');
-  }
-}
-
-Array.from(form.elements).forEach(element => {
-  element.addEventListener('input', () => {
-    validateForm()
-    checkFormIsFilled()
-  })
-});
-
-submitButton.addEventListener('click', handleFormSubmit);
-form.addEventListener('reset', () => {
-  changeButtonState(clearButton, false);
-  changeButtonState(submitButton, false);
-  select.reset();
-  categorySelect.reset();
-  if (resultBlock.classList.contains('block_result_visible')) {
-    resultBlock.classList.remove('block_result_visible');
-  }
-});
-
-/* Copy result */
-
-const copyButton = document.querySelector('#copy-button');
-const resultField = document.querySelector('#result');
-const resultBlock = document.querySelector('.block_result');
-
-function copy() {
-  resultField.select();
-  document.execCommand('copy');
-}
-
-copyButton.addEventListener('click', copy);
-
 /* Custom select */
 
 const select = new CustomSelect(productSelectData);
@@ -164,6 +48,124 @@ getCategoryList().then(data => {
   });
 });
 
+/* Handle form */
+
+const form = document.querySelector('#form');
+
+const submitButton = form.querySelector('#submit-button');
+const clearButton = form.querySelector('#clear-button');
+
+function changeButtonState(button, isFormValid) {
+  if (isFormValid) {
+    button.classList = 'button';
+  } else {
+    button.classList = 'button button_inactive';
+  }
+}
+
+function validateForm() {
+  changeButtonState(submitButton, false);
+
+  let isFormValid = false;
+
+  const requiredElements = form.querySelectorAll('[required]');
+
+  for (let i = 0; i < requiredElements.length; i++) {
+    if (!requiredElements[i].value.length) {
+      isFormValid = false;
+      break;
+    } else {
+      isFormValid = true;
+    }
+  }
+
+  if (form.querySelector('#product-type').checked) {
+    isFormValid = isFormValid && !!form.querySelector('#special-product').value.length;
+  }
+
+  changeButtonState(submitButton, isFormValid);
+  return isFormValid;
+}
+
+function checkFormIsFilled() {
+  const formElements = Array.from(form.elements);
+
+  for (let i = 0; i < formElements.length; i++) {
+    if (
+      formElements[i].type === 'text' && formElements[i].value.length ||
+      formElements[i].type === 'hidden' && formElements[i].value.length ||
+      formElements[i].id === 'product-type' && formElements[i].checked) {
+      changeButtonState(clearButton, true);
+      break;
+    }
+    changeButtonState(clearButton, false);
+  }
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  if (validateForm()) {
+    let result = '';
+    const data = {};
+
+    Array.from(form.elements).forEach((element) => {
+      const { name, type } = element;
+      const value = type === 'checkbox' ? element.checked : element.value.trim();
+      data[name] = value;
+    })
+
+    data.link = trimFromSlash(data.link);
+
+    data.category = replaceHyphen(data.category);
+
+    data['utm-type'] = form.querySelector('input[type=radio]:checked').value;
+
+    if (data['product-type']) {
+      data.product = data['special-product'];
+    } else {
+      data.product = replaceHyphen(getSlug(data.link));
+    }
+
+    result = `${data.link}/?internal_source=tsecrets-${data.category}-${data['utm-type']}-${data.product}`;
+
+    resultField.value = result;
+    resultBlock.classList.add('block_result_visible');
+  }
+}
+
+Array.from(form.elements).forEach(element => {
+  element.addEventListener('input', () => {
+    validateForm();
+    checkFormIsFilled();
+  })
+});
+
+submitButton.addEventListener('click', handleFormSubmit);
+
+form.addEventListener('reset', () => {
+  changeButtonState(clearButton, false);
+  changeButtonState(submitButton, false);
+  select.reset();
+  categorySelect.reset();
+  if (resultBlock.classList.contains('block_result_visible')) {
+    resultBlock.classList.remove('block_result_visible');
+  }
+});
+
+/* Copy result */
+
+const copyButton = document.querySelector('#copy-button');
+const resultField = document.querySelector('#result');
+const resultBlock = document.querySelector('.block_result');
+
+function copy() {
+  resultField.select();
+  document.execCommand('copy');
+}
+
+copyButton.addEventListener('click', copy);
+
 /* Utils */
 
 function trimFromSlash(string) {
@@ -174,5 +176,10 @@ function trimFromSlash(string) {
 }
 
 function getSlug(link) {
-  return link.split('/').slice(-1);
+  return link.split('/').slice(-1).toString();
+}
+
+function replaceHyphen(string) {
+  const regex = /\-/gm;
+  return string.replace(regex, '_');
 }
